d6b1c261e51f4b4cc96a2906d4cdf4eff19303d1
Patrick of AdDU <patrick.paasa@gmail.com>
2021-02-03 14:07:03 +0800

    samples for PA#2

--- js/my.js
+++ js/my.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('../index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.end(data);
+        }
+    });
+});
+
+server.listen(3000, "localhost", () => {
+    console.log("listening at port 3000");
+})
